57ee3de7850ab13cdbfee8654430e5e47e47d0ba
Matt Ellis <melliss67@yahoo.com>
2015-07-11 12:56:47 -0700

    still having problems with gmail authentication.
    added tree control for navigation

--- application.py
+++ application.py
@@ -38,14 +38,14 @@ def list_categories():
 	returnString = '<ul>'
 	categories = session.query(Categories).all()
 	for c in categories:
-		returnString = returnString + '<li>' + c.name + '</li><ul>'
+		returnString = returnString + '<li data-jstree=\'{"opened":false,"selected":false,"url":"#"}\'>' + c.name + '<ul>'
 		subcategories = session.query(Subcategories).filter_by(category_id = c.id)
 		for s in subcategories:
-			returnString = returnString + '<li>' + s.name + '</li>'
+			returnString = returnString + '<li data-jstree=\'{"url":"http://www.purple.com/faq.html"}\'>' + s.name + '</li>'
 			
 		
 		returnString = returnString + '</ul>'
-	returnString = returnString + '</ul>'
+	returnString = returnString + '</li></ul>'
 	return returnString
 
 # User Helper Functions
@@ -74,18 +74,16 @@ def getUserID(email):
 	
 @app.route('/')
 def showItems():
-	if 'username' not in login_session:
-		return redirect('/login')
-	categories = session.query(Categories).all()
-	subcategories = session.query(Subcategories).all()
+	# if 'username' not in login_session:
+		# return redirect('/login')
+	# categories = session.query(Categories).all()
+	# subcategories = session.query(Subcategories).all()
 	categories_list = list_categories()
-	return render_template('items.html',categories = categories, subcategories = subcategories, categories_list = categories_list, access_token = login_session['access_token'])
-	
-    #restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
-    #if 'username' not in login_session:
-        #return render_template('publicrestaurants.html', restaurants=restaurants)
-    #else:
-        #return render_template('restaurants.html', restaurants=restaurants)
+	return render_template('items.html', categories_list = categories_list, user = login_session.get('username'), access_token = login_session.get('access_token'), credentials = login_session.get('credentials'))
+
+@app.route('/test')
+def test():
+	return render_template('test.html')
 	
 @app.route('/login')
 def login():
@@ -103,22 +101,23 @@ def gconnect():
         return response
     # Obtain authorization code
     code = request.data
-    #return code
 
     try:
         # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
+        return credentials.access_token
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Check that the access token is valid.
     access_token = credentials.access_token
-    login_session['access_token'] = access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # If there was an error in the access token info, abort.
@@ -145,22 +144,22 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),200)
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
         response.headers['Content-Type'] = 'application/json'
         return response
+
     # Store the access token in the session for later use.
-	# login_session['access_token'] = access_token
-    # login_session['credentials'] = credentials
-    # return code
+    login_session['credentials'] = credentials
     login_session['gplus_id'] = gplus_id
 
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-	
+
     data = answer.json()
-	
+
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
@@ -187,13 +186,13 @@ def gconnect():
 @app.route('/gdisconnect')
 def gdisconnect():
     # Only disconnect a connected user.
-    access_token = login_session.get('access_token')
-    #if access_token is None:
-        #response = make_response(
-            #json.dumps('Current user not connected.'), 401)
-        #response.headers['Content-Type'] = 'application/json'
-        #return response
-    # access_token = credentials.access_token
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials.access_token
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
